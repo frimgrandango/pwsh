cb0c17b2bef58d06fb683f02b6c0f02174da7ff4
Croffy <45387246+frimgrandango@users.noreply.github.com>
2019-09-13 16:02:10 +1000

    Create recent_winEvents.ps1

---
+++ recent_winEvents.ps1
@@ -0,0 +1,21 @@
+#Short script to pull recent windows event entries from a machine
+
+#time of your event
+$start_time = "9/7/2019 12:34:37 AM"
+$end_time = "9/7/2019 12:35:37 AM"
+
+#get all event logs
+$event_logs = Get-WinEvent -ListLog * -EA silentlycontinue | where-object { $_.recordcount -gt 0} 
+
+#filter through list to find events
+foreach ($event_log in $event_logs) {
+    "Seraching {0}" -f $event_log.LogName
+    try
+    {
+       get-winevent -FilterHashtable @{LogName=$event_log.LogName;StartTime=$time;EndTime=$end_time} -ErrorAction SilentlyContinue
+    }
+    catch
+    {
+     "An Error has occurred"
+    }
+ }
